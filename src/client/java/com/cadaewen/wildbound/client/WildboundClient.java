13113e259f7a900c5910506bd1c2759151b80693
Cadaewen <atperry7@gmail.com>
2026-06-08 10:05:09 -0400

    Initial Commit

---
+++ src/client/java/com/cadaewen/wildbound/client/WildboundClient.java
@@ -0,0 +1,10 @@
+package com.cadaewen.wildbound.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WildboundClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
